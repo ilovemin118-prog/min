44912a8cfe04a56404628a921c8def464c7e8072
min <ilovemin118@gmail.com>
2026-04-18 13:35:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let Shownum = document.getElementById('Show-num');
+let val1 = "";
+let val2 = "";
+let po = "";
+
+function shownum(num) {
+    if (num === "C") {
+        val1 = ""; val2 = ""; po = "";
+        Shownum.innerText = "0";
+        return;
+    }
+
+    if (num === 'X' || num === '-' || num === '+') {
+        po = num === 'X' ? '*' : num;
+        Shownum.innerText = po;  // ✅ แสดง operator บนหน้าจอ
+        return;
+    }
+
+    if (num === "=") {
+        let re;
+        if (po === "*") re = Number(val1) * Number(val2);
+        if (po === "-") re = Number(val1) - Number(val2);
+        if (po === "+") re = Number(val1) + Number(val2);
+        Shownum.innerText = re;
+        val1 = String(re);  // ✅ เอาผลลัพธ์ไปต่อได้เลย
+        val2 = "";
+        po = "";
+        return;
+    }
+
+    if (po === "") {
+        val1 += num;
+        Shownum.innerText = val1;
+    } else {
+        val2 += num;
+        Shownum.innerText = val2;
+    }
+}
